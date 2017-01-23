6a07c6ea801db2ec7bc79da0595cd7290833825b
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2017-01-23 21:34:50 +0900

    js to web-mode contennt-type: jsx

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -228,7 +228,9 @@
 ;; js/jsx
 ;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-jsx-mode))
 ;; (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(setq web-mode-content-types-alist
+  '(("jsx" . "\\.js[x]?\\'")))
 ;; disable jshint since we prefer eslint checking
 (setq-default flycheck-disabled-checkers
               '(javascript-jshint))
