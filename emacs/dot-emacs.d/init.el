c93813486cfc9efb7bb52c384b82f37114869ddf
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2024-04-13 00:24:48 +0900

    Emacs minor modes

--- emacs/dot-emacs.d/init.el
+++ emacs/dot-emacs.d/init.el
@@ -19,12 +19,16 @@
 ;; Configure use-package to use straight.el by default
 (use-package straight
   :custom (straight-use-package-by-default t))
-(use-package better-defaults)
+(use-package better-defaults
+  :config (scroll-bar-mode t))
 (use-package zenburn-theme
-  :config (load-theme 'zenburn t))
+  :config (load-theme 'zenburn t)
+  (set-face-attribute 'region nil :background "#666" :foreground "#ffffff"))
 (use-package exec-path-from-shell
   :if (memq window-system '(mac ns))
-  :config (exec-path-from-shell-initialize))
+  :config (exec-path-from-shell-initialize)
+  (add-to-list 'default-frame-alist '(alpha . 80))
+  :custom (initial-frame-alist '((width . 165)(height . 45)(top . 50)(left . 50))))
 (use-package flx-ido)
 (use-package ido-completing-read+)
 (use-package ido-vertical-mode)
@@ -41,14 +45,32 @@
 (use-package company)
 (use-package flycheck)
 (use-package flycheck-color-mode-line)
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
 (use-package smex)
 (use-package yasnippet)
 (use-package magit)
 ;; eglot
 (use-package eglot
  :hook ((go-mode ruby-mode) . eglot-ensure))
+;; minor modes
+(use-package highlight-indent-guides
+  :hook ((text-mode prog-mode) . highlight-indent-guides-mode)
+  :custom ((highlight-indent-guides-method 'character)
+           (highlight-indent-guides-character ?|)
+           (highlight-indent-guides-auto-character-face-perc 70)
+           (highlight-indent-guides-responsive 'top)
+           (highlight-indent-guides-auto-top-character-face-perc 100)))
+(use-package mwim
+  :bind (("C-a" . mwim-beginning)
+         ("C-e" . mwim-end)))
+(use-package rainbow-delimiters
+  :hook ((text-mode prog-mode) . rainbow-delimiters-mode))
+(use-package smartparens
+  :disabled
+  :hook ((text-mode prog-mode) . smartparens-mode)(after-init . smartparens-global-strict-mode)
+  :config (require 'smartparens-config))
+(use-package whitespace
+  :config (global-whitespace-mode t)
+  :custom (whitespace-style '(tab-mark)))
 ;; major modes
 (use-package elixir-mode)
 (use-package go-mode
@@ -85,26 +107,6 @@
 (when (functionp 'mac-auto-ascii-mode)
   (mac-auto-ascii-mode 1))
 
-;;emacs version/system oriented
-(if window-system
-    (progn
-      (add-to-list 'default-frame-alist '(alpha . 80))
-      (setq initial-frame-alist '((width . 165)(height . 45)(top . 50)(left . 50)))
-      ;; (load-theme 'solarized-dark t)
-      ;; (load-theme 'zenburn t)
-      (set-face-attribute 'region nil :background "#666" :foreground "#ffffff")
-      ;; (load-theme 'wombat t)
-      ;; (cond
-      ;;  ;; cocoa emacs
-      ;;  ((eq window-system 'ns)
-      ;;   (setq mac-command-modifier 'meta)
-      ;;   (setq mac-allow-anti-aliasing t)
-      ;;   (setq yank-excluded-properties t)
-      ;;   (setq exec-path-from-shell-check-startup-files nil)
-      ;;   (exec-path-from-shell-initialize)
-      ;;   ;; (load "emacs23mac_font.el")
-      ;;   ))
-      ))
 ;;tabs, indent
 (setq standard-indent 2)
 (setq-default tab-width 2)
@@ -141,15 +143,10 @@
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
 
-;; ;; C-h でカーソルの左にある文字を消す
-;; (define-key global-map "\C-h" 'delete-backward-char)
-;; ;; C-h に割り当てられている関数 help-command を C-x C-h に割り当てる
-;; (define-key global-map "\C-x\C-h" 'help-command)
-
 ;; リージョンを色付きにする
 (setq-default transient-mark-mode 1)
 ;; 時間を表示
-(display-time-mode 1)
+(display-time-mode -1)
 ;; 列数表示
 (column-number-mode 1)
 ; startup message disable
@@ -208,14 +205,6 @@
 ;;(global-set-key (kbd "C-c C-c") 'comment-dwim)
 (global-set-key (kbd "M-c") 'kill-ring-save)
 
-;; anything -> helm
-;; (require 'helm-config)
-;; (add-to-list 'load-path "~/Dropbox/dotfiles/elisp/anything-config/")
-;;(require 'anything-startup)
-;; (require 'anything-config)
-;; (global-set-key (kbd "M-x") 'helm-M-x)
-;; (setq recentf-max-saved-items 500)
-
 ;; yasnippet
 ;; (add-to-list 'load-path "~/Dropbox/dotfiles/elisp/yasnippet/")
 ;; (require 'yasnippet) ;; not yasnippet-bundle
@@ -247,46 +236,9 @@
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
   (define-key company-active-map [tab] 'company-complete-selection) ;; TABで候補を設定
   (define-key company-active-map (kbd "C-g") 'company-abort)
-  ;; (define-key company-active-map (kbd "C-h") nil) ;; C-hはバックスペース割当のため無効化
-  ;; (define-key company-active-map (kbd "C-S-h") 'company-show-doc-buffer) ;; ドキュメント表示はC-Shift-h
   )
 
-;; (defun my-anything ()
-;;   (interactive)
-;;   (anything-other-buffer
-;;    '(anything-c-source-buffers+
-;;      anything-c-source-files-in-current-dir+
-;;      anything-c-source-recentf
-;;      anything-c-source-kill-ring
-;;      anything-c-source-yasnippet
-;;      anything-c-source-find-files
-;;      anything-c-source-file-name-history
-;;      anything-c-source-complex-command-history
-;;      anything-c-source-emacs-commands
-;;      anything-c-source-google-suggest
-;; ;;     anything-c-source-info-pages
-;; ;;      anything-c-source-info-elisp
-;; ;;      anything-c-source-man-pages
-;; ;;     anything-c-source-locate
-;;      )
-;;    " *my-anything*"))
-;; (global-set-key (kbd "C-;") 'my-anything)
-;; (defun my-mark-rings ()
-;;   (interactive)
-;;   (anything-other-buffer
-;;    '(
-;;      anything-c-source-global-mark-ring
-;;      anything-c-source-mark-ring
-;;      anything-c-source-kill-ring
-;;      anything-c-source-yasnippet
-;;      )
-;;    " *my-mark-rings*"))
-;; (global-set-key (kbd "M-'") 'my-mark-rings)
-
-
-
 ;; modes
-
 
 ;; js/jsx
 ;; https://github.com/ananthakumaran/tide/
@@ -359,59 +311,5 @@
 ;; remove whitespace at the last of the line on save.
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;====================================
-;;全角スペースとかに色を付ける
-;====================================
-(defface my-face-b-1 '((t (:background "medium aquamarine"))) nil)
-(defface my-face-b-1 '((t (:background "dark turquoise"))) nil)
-;(defface my-face-b-2 '((t (:background "cyan"))) nil)
-(defface my-face-b-2 '((t (:background "DimGray"))) nil)
-(defface my-face-b-2 '((t (:background "SeaGreen"))) nil)
-(defface my-face-u-1 '((t (:foreground "SteelBlue" :underline t))) nil)
-(defvar my-face-b-1 'my-face-b-1)
-(defvar my-face-b-2 'my-face-b-2)
-(defvar my-face-u-1 'my-face-u-1)
-(defadvice font-lock-mode (before my-font-lock-mode ())
-  (font-lock-add-keywords
-   major-mode
-   '(
-     ("　" 0 my-face-b-1 append)
-     ("\t" 0 my-face-b-2 append)
-     ("[ ]+$" 0 my-face-u-1 append)
-     )))
-(ad-enable-advice 'font-lock-mode 'before 'my-font-lock-mode)
-(ad-activate 'font-lock-mode)
-(add-hook 'find-file-hooks '(lambda ()
-                              (if font-lock-mode
-                                  nil
-                                (font-lock-mode t))))
-
 ;; 自動改行の調整
 (setq truncate-partial-width-windows nil)
-
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(js-switch-indent-offset 2)
-;;  '(js2-basic-offset 2)
-;;  '(js2-indent-switch-body t)
-;;  '(js2-mode-indent-ignore-first-tab t)
-;;  '(js2-strict-missing-semi-warning nil)
-;;  '(js2-strict-trailing-comma-warning nil)
-;;  '(package-selected-packages
-;;    (quote
-;;     (rjsx-mode tide company typescript-mode php-mode terraform-mode graphviz-dot-mode coffee-mode ido-vertical-mode ido-yes-or-no flx-ido projectile-rails zenburn-theme yasnippet yard-mode yaml-mode web-mode use-package solarized-theme smex smartparens slim-mode scss-mode rspec-mode rainbow-delimiters projectile prodigy popwin pallet nyan-mode nginx-mode multiple-cursors markdown-toc magit js2-mode idle-highlight-mode htmlize golden-ratio go-mode flycheck-color-mode-line flycheck-cask feature-mode expand-region exec-path-from-shell elixir-mode edit-server drag-stuff csv-mode better-defaults)))
-;;  '(rspec-spec-command "bin/rspec")
-;;  '(rspec-use-bundler-when-possible nil)
-;;  '(rspec-use-rake-when-possible nil)
-;;  '(rspec-use-spring-when-possible nil)
-;;  '(web-mode-attr-indent-offset 2))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
-;; (add-hook 'after-init-hook 'inf-ruby-switch-setup)
